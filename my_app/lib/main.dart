a6446611e86f7b703dfecdc5a657d545ac705259
itsvivekghosh <soapmactevis1@gmail.com>
2020-08-17 16:57:09 +0530

    added List Project

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -2,102 +2,28 @@ import 'package:flutter/material.dart';
 
 void main() {
   return runApp(MaterialApp(
-    home: Home(),
+    home: QuoteList(),
   ));
 }
 
-class Home extends StatefulWidget {
+class QuoteList extends StatefulWidget {
   @override
-  _HomeState createState() => _HomeState();
+  _QuoteListState createState() => _QuoteListState();
 }
 
-class _HomeState extends State<Home> {
-  int ninjaLevel = 0;
-
+class _QuoteListState extends State<QuoteList> {
+  List<String> quotes = ["hello vivek", "scdsc", "cscdsc", "cscsdc"];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[900],
+      backgroundColor: Colors.grey[400],
       appBar: AppBar(
-        title: Text("Ninja Bar"),
+        title: Text("Awesome Quotes".toUpperCase()),
         centerTitle: true,
+        titleSpacing: 1.0,
         backgroundColor: Colors.grey[850],
-        elevation: 0.2,
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            ninjaLevel += 1;
-          });
-          print("Hello Ninja Level: $ninjaLevel");
-        },
-        child: Icon(Icons.add),
-        backgroundColor: Colors.grey[800],
-      ),
-      body: Padding(
-        padding: EdgeInsets.fromLTRB(30, 20, 40, 0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Center(
-                child: CircleAvatar(
-              backgroundImage: AssetImage('assets/default.jpg'),
-              radius: 40.0,
-            )),
-            Divider(height: 100, color: Colors.grey[800]),
-            Text(
-              "NAME",
-              style: TextStyle(color: Colors.grey, letterSpacing: 2.0),
-            ),
-            SizedBox(
-              height: 10.0,
-            ),
-            Text(
-              "Vivek Ghosh",
-              style: TextStyle(
-                  fontSize: 28.0,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.amberAccent[200],
-                  letterSpacing: 2.0),
-            ),
-            SizedBox(
-              height: 30.0,
-            ),
-            Text(
-              "NINJA LEVEL",
-              style: TextStyle(color: Colors.grey, letterSpacing: 2.0),
-            ),
-            SizedBox(
-              height: 10.0,
-            ),
-            Text(
-              "$ninjaLevel",
-              style: TextStyle(
-                  fontSize: 28.0,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.amber[200],
-                  letterSpacing: 2.0),
-            ),
-            SizedBox(
-              height: 30.0,
-            ),
-            Row(
-              children: [
-                Icon(
-                  Icons.email,
-                  color: Colors.white,
-                ),
-                SizedBox(width: 10),
-                Text("soapmactevis1@gmail.com",
-                    style: TextStyle(
-                        color: Colors.grey[400],
-                        fontSize: 18.0,
-                        letterSpacing: 1.0)),
-              ],
-            )
-          ],
-        ),
-      ),
+      body: Column(children: quotes.map((quote) => Text(quote)).toList()),
     );
   }
 }
