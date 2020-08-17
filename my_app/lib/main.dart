e19aa53c0126ffffd151e88b02be3f65cf0bdf02
itsvivekghosh <soapmactevis1@gmail.com>
2020-08-17 14:13:21 +0530

    added changes

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -1,34 +1,51 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text("My First App"),
-      centerTitle: true,
-      backgroundColor: Colors.redAccent,
-    ),
-    body: Center(
-      child: Text(
-          "Hello From Vivek!",
-          style: TextStyle(
-            fontSize: 20.0,
-            fontWeight: FontWeight.bold,
-            letterSpacing: 2.0,
-            color: Colors.grey[400],
-            fontFamily: "Roboto-Monot"
-      )),
-    ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () { },
-        child: Text("Click"),
-        backgroundColor: Colors.redAccent[400],
-      ),
-  ),
-));
+      home: Home(),
+    ));
 
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("My First App"),
+        centerTitle: true,
+        backgroundColor: Colors.redAccent,
+      ),
+      body: Row(
+        children: [
+          Expanded(child: Image.asset('assets/image1.jpg'), flex: 2),
+          Expanded(
+            flex: 1,
+            child: Container(
+                padding: EdgeInsets.all(30.0),
+                color: Colors.yellowAccent,
+                child: Text("1")),
+          ),
+          Expanded(
+            flex: 1,
+            child: Container(
+              padding: EdgeInsets.all(30.0),
+              color: Colors.grey[400],
+              child: Text("2"),
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Container(
+              padding: EdgeInsets.all(30.0),
+              color: Colors.cyan,
+              child: Text("3"),
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Text("Click"),
+        backgroundColor: Colors.redAccent[400],
+      ),
+    );
   }
 }
