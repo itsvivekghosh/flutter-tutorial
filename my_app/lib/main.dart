959c2fbaf7d1f0d43cfe742fb8fc3d2b8b50f246
itsvivekghosh <soapmactevis1@gmail.com>
2020-08-17 14:45:22 +0530

    first basic application

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -10,9 +10,76 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[900],
       appBar: AppBar(
         title: Text("Ninja Bar"),
         centerTitle: true,
+        backgroundColor: Colors.grey[850],
+        elevation: 0.2,
+      ),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30, 20, 40, 0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Center(
+                child: CircleAvatar(
+              backgroundImage: AssetImage('assets/default.jpg'),
+              radius: 40.0,
+            )),
+            Divider(height: 100, color: Colors.grey[800]),
+            Text(
+              "NAME",
+              style: TextStyle(color: Colors.grey, letterSpacing: 2.0),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text(
+              "Vivek Ghosh",
+              style: TextStyle(
+                  fontSize: 28.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.amberAccent[200],
+                  letterSpacing: 2.0),
+            ),
+            SizedBox(
+              height: 30.0,
+            ),
+            Text(
+              "Current Ninja Level",
+              style: TextStyle(color: Colors.grey, letterSpacing: 2.0),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text(
+              "8",
+              style: TextStyle(
+                  fontSize: 28.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.amber[200],
+                  letterSpacing: 2.0),
+            ),
+            SizedBox(
+              height: 30.0,
+            ),
+            Row(
+              children: [
+                Icon(
+                  Icons.email,
+                  color: Colors.white,
+                ),
+                SizedBox(width: 10),
+                Text("soapmactevis1@gmail.com",
+                    style: TextStyle(
+                        color: Colors.grey[400],
+                        fontSize: 18.0,
+                        letterSpacing: 1.0)),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
