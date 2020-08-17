15576bf4061d79a8e524cf7c6f8dd60d6df1fe13
itsvivekghosh <soapmactevis1@gmail.com>
2020-08-17 14:19:49 +0530

    init application changes

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -1,50 +1,18 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-      home: Home(),
-    ));
+void main() {
+  return runApp(MaterialApp(
+    home: Home(),
+  ));
+}
 
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("My First App"),
+        title: Text("Ninja Bar"),
         centerTitle: true,
-        backgroundColor: Colors.redAccent,
-      ),
-      body: Row(
-        children: [
-          Expanded(child: Image.asset('assets/image1.jpg'), flex: 2),
-          Expanded(
-            flex: 1,
-            child: Container(
-                padding: EdgeInsets.all(30.0),
-                color: Colors.yellowAccent,
-                child: Text("1")),
-          ),
-          Expanded(
-            flex: 1,
-            child: Container(
-              padding: EdgeInsets.all(30.0),
-              color: Colors.grey[400],
-              child: Text("2"),
-            ),
-          ),
-          Expanded(
-            flex: 1,
-            child: Container(
-              padding: EdgeInsets.all(30.0),
-              color: Colors.cyan,
-              child: Text("3"),
-            ),
-          ),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Text("Click"),
-        backgroundColor: Colors.redAccent[400],
       ),
     );
   }
