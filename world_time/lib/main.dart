d6a4cd3977e6847594a95aec8e2abfc9c3b44f92
itsvivekghosh <soapmactevis1@gmail.com>
2020-08-18 00:56:57 +0530

    added datetime

--- world_time/lib/main.dart
+++ world_time/lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
-import 'pages/home.dart';
+import 'package:world_time/pages/loading.dart';
+import 'package:world_time/pages/home.dart';
+import 'package:world_time/pages/choose_location.dart';
 
 void main() {
-  return runApp(MaterialApp(home: Home()));
+  return runApp(MaterialApp(initialRoute: '/', routes: {
+    '/': (context) => Loading(),
+    '/home': (context) => Home(),
+    '/location': (context) => ChooseLocation(),
+  }));
 }
--- world_time/lib/pages/choose_location.dart
+++ world_time/lib/pages/choose_location.dart
@@ -8,6 +8,20 @@ class ChooseLocation extends StatefulWidget {
 class _ChooseLocationState extends State<ChooseLocation> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Text("Choose Loaction"));
+    print("Build is called");
+
+    return Scaffold(
+        backgroundColor: Colors.grey[200],
+        appBar: AppBar(
+          backgroundColor: Colors.blue[900],
+          title: Text("Choose Location"),
+          centerTitle: true,
+          elevation: 0.2,
+        ),
+        body: RaisedButton(
+            onPressed: () {
+              setState(() {});
+            },
+            child: Text("Click Me")));
   }
 }
